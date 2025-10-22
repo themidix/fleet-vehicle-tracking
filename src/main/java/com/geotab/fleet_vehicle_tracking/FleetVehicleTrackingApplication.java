be31929b58c8e1b2ee9b6e0953351dfcb861cdcd
themidix <midix@gmail.com>
2025-10-22 19:54:00 -0400

    first commit

---
+++ src/main/java/com/geotab/fleet_vehicle_tracking/FleetVehicleTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.geotab.fleet_vehicle_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetVehicleTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetVehicleTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geotab/fleet_vehicle_tracking/FleetVehicleTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geotab.fleet_vehicle_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetVehicleTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
